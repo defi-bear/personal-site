a29807f92f83d127be9c5ba200d501e60d8d3d9b
Shing Ho Tan <talenteyedev@gmail.com>
2022-03-11 07:37:03 -0500

    overview part

--- pages/parts/Overview.js
+++ pages/parts/Overview.js
@@ -4,6 +4,21 @@ import Card from "pages/components/Card";
 import CursorButton from "pages/components/CursorButton";
 import styles from "styles/Overview.module.scss";
 
+const skills = [
+  "JavaSciprt",
+  "React.js",
+  "React-Native",
+  "Node.js",
+  "TypeScript",
+  "Python",
+  "Django",
+  "PHP",
+  "Laravel",
+  "Java",
+  "Rust",
+  "Ruby on Rails",
+];
+
 function Overview() {
   return (
     <div className={styles.container}>
@@ -46,7 +61,7 @@ function Overview() {
             <div className={styles.overviewSecond}>
               Well-versed in numerous prog ramming languages and frameworks
               including JavaScript, React.js, React- Native, Vue.js, Python,
-              Django, Node.js, Java, Spring-boot, ASP.NET. Strong background in
+              Django, Node.js, Java, Spring- boot, ASP.NET. Strong background in
               project management and customer relations.
             </div>
             <CursorButton style={styles.clickButton}>
@@ -77,6 +92,18 @@ function Overview() {
               height="830"
               className={styles.pfp}
             />
+          </div>
+          <div className={styles.skillsContainer}>
+            <div className={styles.skillsHeader}>
+              <div className={styles.redBut} />
+              <div className={styles.yellowBut} />
+              <div className={styles.greenBut} />
+            </div>
+            {skills.map((skill) => (
+              <div className={styles.skill} key={skill}>
+                {skill}
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- pages/parts/Skills.js
+++ pages/parts/Skills.js
@@ -1,5 +1,7 @@
+import styles from "styles/Skills.module.scss";
+
 function Skills() {
-  return <div>Skills</div>;
+  return <div className={styles.container}>Skills</div>;
 }
 
 export default Skills;
