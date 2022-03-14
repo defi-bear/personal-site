00f7c227a997111e187ed91e40c34dd69ffd4590
Shing Ho Tan <talenteyedev@gmail.com>
2022-03-14 00:27:47 -0400

    experience part

--- pages/parts/Experience.js
+++ pages/parts/Experience.js
@@ -1,5 +1,23 @@
+import Image from "next/image";
+import styles from "styles/Experience.module.scss";
+
+import expList from "utils/experience";
+
 function Experience() {
-  return <div>Experience</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.illustration}>
+        <Image
+          alt="Illustration"
+          src="/assets/svg/exp_illustration.svg"
+          width="100"
+          height="130"
+        />
+      </div>
+      <div className={styles.label}>Experience</div>
+      <div className={styles.body}></div>
+    </div>
+  );
 }
 
 export default Experience;
