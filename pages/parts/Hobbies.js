f22c2b668ff19f2b46d121dd33622a8acf187d2a
Shing Ho Tan <talenteyedev@gmail.com>
2022-03-14 23:47:41 -0400

    hobbies complete

--- pages/parts/Hobbies.js
+++ pages/parts/Hobbies.js
@@ -1,11 +1,43 @@
+import { Flex, Box } from "@chakra-ui/react";
 import Image from "next/image";
 import styles from "styles/Hobbies.module.scss";
+import Card from "pages/components/Card";
 
 function Hobbies() {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <div className={styles.label}>Hobbies</div>
+        <div className={styles.hobbiesList}>
+          <Flex mt="17" ml="20px" direction="row">
+            <Card style={styles.card}>Dota 2</Card>
+            <Card style={styles.card}>Swimming</Card>
+            <Image
+              src="/assets/svg/blue_frame.svg"
+              width="200"
+              height="65"
+              alt="Blue Frame"
+            />
+          </Flex>
+          <Flex mt="17" direction="row">
+            <Image
+              src="/assets/svg/frame.svg"
+              width="200"
+              height="65"
+              alt="Frame"
+            />
+            <Box ml="3">
+              <Card style={styles.card}>Basketball</Card>
+            </Box>
+          </Flex>
+          <Flex mt="17" ml="40px" direction="row">
+            <Card style={styles.card}>Cooking and Reading books</Card>
+          </Flex>
+          <Flex mt="17" ml="20px" direction="row">
+            <Card style={styles.card}>Tabletennis</Card>
+            <Card style={styles.card}>Watching Film</Card>
+          </Flex>
+        </div>
       </div>
       <div className={styles.right}>
         <Image
