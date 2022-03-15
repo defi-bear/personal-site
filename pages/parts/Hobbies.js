3514245d76bb13b8b19bb5154517c974a17d7d12
Shing Ho Tan <talenteyedev@gmail.com>
2022-03-14 21:22:39 -0400

    hobbies init

--- pages/parts/Hobbies.js
+++ pages/parts/Hobbies.js
@@ -1,5 +1,22 @@
+import Image from "next/image";
+import styles from "styles/Hobbies.module.scss";
+
 function Hobbies() {
-  return <div>Hobbies</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <div className={styles.label}>Hobbies</div>
+      </div>
+      <div className={styles.right}>
+        <Image
+          src="/assets/svg/computer.svg"
+          width="514"
+          height="521"
+          alt="Computer"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Hobbies;
